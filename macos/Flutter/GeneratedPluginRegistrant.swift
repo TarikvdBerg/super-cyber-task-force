150b57f0501873a36dea22bd9508d51a5f7d566b
Niels van Marion <niels.van.marion@quicknet.nl>
2020-02-26 11:19:26 +0100

    Fixed base application

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import color_panel
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  ColorPanelPlugin.register(with: registry.registrar(forPlugin: "ColorPanelPlugin"))
 }
